8ba329a11afa1dabd600104aadeda4ffff60fbfa
Ismael Hernandez <ismaelh@alianzatrayecto.com>
2020-11-25 00:27:27 -0600

    ejemplo bootstrap

--- routes/web.php
+++ routes/web.php
@@ -16,4 +16,5 @@ use Illuminate\Support\Facades\Route;
 // Route::get('/', function () {
 //     return view('welcome');
 // });
-Route::view('/', 'inicio')->name('inicio');
+Route::view('/', 'Login')->name('Login');
+Route::view('/Inicio', 'inicio')->name('inicio');
